efa05133b64c63b40a2b9131ab684863989656e0
Sanjay-RD <rishidevsanjay9@gmail.com>
2026-09-09 12:15:10 +0545

    9s added

--- app/portfolio/eka-scvs/page.tsx
+++ app/portfolio/eka-scvs/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     <VehicleListingPage
       title="Small commercial vehicles"
       description="Practical electric vehicles for deliveries, cargo and passenger mobility in busy urban areas."
-      ids={["eka-1-5t", "eka-3w-cargo", "eka-6s", "eka-3s"]}
+      ids={["eka-1-5t", "eka-3w-cargo", "eka-6s", "eka-6s", "eka-3s"]}
     />
   );
 }
